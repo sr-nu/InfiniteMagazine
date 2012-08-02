4b09cfdedd383a08d3af680a93ede1e02624eedf
Srikanth <srikanth.nutigattu@gmail.com>
2012-08-02 17:45:02 +0530

    New Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InfiniteMagazine::Application.config.secret_token = 'd073e6d68dc44100974b1047391a7674d357fbd4b6c9fc87aa67eb85a8320fc4e7370f30fc126cfcf33092f7c04196c72f91e1f227e0396103944cd26e61e82f'
