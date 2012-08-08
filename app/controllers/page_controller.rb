72aaadcdc57fa1a6ce7418227cf195b9f4071a36
Srikanth <srikanth.nutigattu@gmail.com>
2012-08-08 16:05:40 +0530

    cleanup

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -4,12 +4,11 @@ require 'open-uri'
 require 'feedzirra'
 class PageController < ApplicationController
 
-	def index
-		@feeds = read_feed_zirra
-	end
+def index
+	@feeds = read_feed_zirra
+end
 
 private
-
 	def read_feed_zirra
 		feed = Feedzirra::Feed.fetch_and_parse("http://news.google.com/news?q=apple&output=rss")
 		feed.entries.collect { |entry| entry.summary }
