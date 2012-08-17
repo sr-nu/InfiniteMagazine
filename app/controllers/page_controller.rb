c1bbb6a4ce15913aef1a339d60ec1f756cc9762e
Srikanth <srikanth.nutigattu@gmail.com>
2012-08-18 02:08:30 +0530

    styling

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -10,8 +10,8 @@ end
 private
 	def read_feed_zirra
 		feed = Feedzirra::Feed.fetch_and_parse("http://news.google.com/news?q=apple&output=rss&num=200")
-		# feed = Feedzirra::Feed.fetch_and_parse("http://www.wired.com/magazine/feed/")
-		feed.entries.collect { |entry| entry.summary  }
+		# feed2 = Feedzirra::Feed.fetch_and_parse("http://www.wired.com/magazine/feed/")
+		feed.entries.collect { |entry| entry.content || entry.summary }
 	end
 end
 
